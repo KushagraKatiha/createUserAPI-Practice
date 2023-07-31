1b2fe3d9ed7339c04f13ec0582e74816103795e9
KushagraKatiha <kushagrakatiha123@gmail.com>
2023-07-31 15:14:07 +0530

    db connection commited

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express');
 const myRoutes = require('./routes/myRoutes.js')
+const connectToDB = require('./config/db.js')
 
 const app = express();
+
+connectToDB();
 
 app.use ('/', myRoutes)
 
